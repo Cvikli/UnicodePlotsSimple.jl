3a8f51d7f7acfb82ae01d972a3486a2f71a04e8b
Marcell <havlikmarcell@gmail.com>
2022-03-03 09:05:05 +0100

    heatmap! ;)

--- src/UnicodeHistogram.jl
+++ src/UnicodeHistogram.jl
@@ -1,7 +1,51 @@
 module UnicodeHistogram
 
+using Crayons
 export histogram
 export distribution
+export heatmap
+
+cpad(s, n::Integer, p=" ") = rpad(lpad(s,div(n+length(s),2),p),n,p)
+function heatmap(xy::Matrix; title=[], xticks=[], yticks=[]) 
+	heatmap([xy], title=title, xticks=xticks, yticks=yticks) 
+end
+function heatmap(xy::Vector; title=[], xticks=[], yticks=[], reversey=false) 
+	ϵ = 1e-11
+	scale = 0:5:255
+	charts = length(xy)
+	width, height = size(xy[1])
+	unicode_matrix = Array{UInt8,3}(undef, width, height, charts)	
+	maxstrength = maximum(vcat(xy...))
+	minstrength = minimum(vcat(xy...))
+	for c in 1:charts
+		for xi in size(xy[c],1)
+			for yi in size(xy[c],2)
+				strength = (xy[c][xi,yi]-minstrength) / (maxstrength-minstrength + ϵ) * length(scale)
+				unicode_matrix[xi,yi,c] = scale[floor(Int, strength)+1]
+			end
+		end
+	end
+	
+	length(title)!= 0 && (print(" ");for c in 1:charts
+		print(cpad(title[c],2*length(xticks)))
+		print(" ")
+	end;
+	println())
+	for h in 1:height 	
+		length(yticks) != 0 && print(yticks[reversey ? height+1-h : h])
+		for c in 1:charts
+			for w in 1:width 	
+				print(Crayon(; foreground = (255, unicode_matrix[w,reversey ? height+1-h : h,c], 0)),"██", Crayon(reset=true))
+			end
+			print(" ")
+		end
+		println()
+	end
+	length(xticks) != 0 && (print(" ");for c in 1:charts 
+	 for w in 1:width print(lpad(xticks[w],2)) end ; print(" ")
+end)
+	println()
+end
 
 function distribution(probs; height=3, values=[])
 	ϵ = 1e-11
@@ -48,8 +92,8 @@ end
 
 
 function histogram(values; width=64, height=3, title="", printstat=true)
-	length(values) == 0 && (println("Empty histogram"); return)
 	ϵ = 1e-11
+	length(values) == 0 && (println("Empty histogram"); return)
 	histbars_blocks = [' ', '▁', '▂', '▃', '▄', '▅', '▆', '▇', '█']
 	blocknum = length(histbars_blocks)
 	histbin_codes = fill('█',(blocknum-1)*height, height)
--- test/test.jl
+++ test/test.jl
@@ -4,7 +4,9 @@ includet("../src/UnicodeHistogram.jl")
 using .UnicodeHistogram
 
 # histogram(values, width=64, height=3)
-histogram(randn(1000).+2, width=100, height=5, title="Random num generation histrogram", printstat=true)
+# histogram(randn(1000).+2, width=100, height=5, title="Random num generation histrogram", printstat=true)
 
 
-distribution([0.3,0.2334,0.1,0.8], height=3, values=[0.3,0.2334,0.1,0.8])
+# distribution([0.3,0.2334,0.1,0.8], height=3, values=[0.3,0.2334,0.1,0.8])
+
+heatmap([randn(5,5),randn(5,5),randn(5,5)],title=["first", "s2ec", "32323c"], xticks=[1,2,2,2,3], yticks=[1,2,2,5,3])
